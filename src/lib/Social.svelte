4548aa88e40bd67e961e34e2ceace1e59594ca5c
Michael Lamb <mflambjr@gmail.com>
2022-09-23 02:32:36 -0400

    generate unique id for each comp instance

--- src/lib/Social.svelte
+++ src/lib/Social.svelte
@@ -2,16 +2,14 @@
 	import { Cluster, Icon } from '@realgoatish/svelte-every-layout';
 
 	export let data;
+
+	// generate unique ID for each instance of this component
+	let id = `social-icons-label-${Math.floor(new Date() * Math.random())}`;
 </script>
 
 <div>
-	<h2 class="screen-reader-only" id="social-icons-label">Call or find us on other platforms</h2>
-	<Cluster
-		wrapperElement="ul"
-		justify="space-evenly"
-		space="var(--s-2)"
-		aria={{ labelledby: 'social-icons-label' }}
-	>
+	<h2 class="screen-reader-only" {id}>Call or find us on other platforms</h2>
+	<Cluster wrapperElement="ul" justify="space-evenly" space="var(--s-2)" aria={{ labelledby: id }}>
 		{#each data.items as item}
 			{#if item.text === 'phone'}
 				<!-- <li>
