e172ac8f51840af449e40e1db261c39f0417fdfa
Mike Lamb <mflambjr@gmail.com>
2023-05-18 21:58:16 -0400

    dynamic import

--- src/routes/(studio)/studio/[...catchall]/+page@(studio).svelte
+++ src/routes/(studio)/studio/[...catchall]/+page@(studio).svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import config from '$lib/config/sanity/sanity.config';
-	import SanityStudio from '$lib/utils/SanityStudio.svelte';
+	// import SanityStudio from '$lib/utils/SanityStudio.svelte';
+
+	let SanityStudio;
+
+	onMount(async () => {
+		SanityStudio = (await import('$lib/utils/SanityStudio.svelte')).default;
+	});
 </script>
 
-<SanityStudio {config} />
+<!-- <SanityStudio {config} /> -->
+
+<svelte:component this={SanityStudio} {config} />
