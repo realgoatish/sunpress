09b90627dc3e821fc1a29d5a46d4924c6b76323d
Mike Lamb <mflambjr@gmail.com>
2024-07-30 22:02:09 -0400

    redirect the old menu url to the new one

--- src/routes/(app)/menu/+page.server.ts
+++ src/routes/(app)/menu/+page.server.ts
@@ -1,31 +1,12 @@
-import { error } from '@sveltejs/kit';
-import { menuPageQuery, menuPagePreviewQuery } from '$lib/config/sanity/queries'
-import { processMenuPage } from '$lib/js/processEndpoints.server'
+
+import { error, redirect } from '@sveltejs/kit';
+
 import type { PageServerLoad } from './$types';
-import { getSanityServerClient } from '$lib/config/sanity/sanityClient.server';
 
-export const load: PageServerLoad = async ({ parent }) => {
-  const { previewMode } = await parent()
+export const load: PageServerLoad = async () => {
 
-  let response
+  throw redirect(308, '/menus/restaurant/')
 
-  if (previewMode) {
-    response = await getSanityServerClient(previewMode).fetch(menuPagePreviewQuery)
-  } else if (!previewMode) {
-    response = await getSanityServerClient(previewMode).fetch(menuPageQuery)
-  }
-
-  if (!response) {
-    error(404, 'Menu Page not found');
-  }
-
-  const processedResponse = processMenuPage(response)
-
-  // SK adds stuff from the layout to this obj for us
-  return {
-    previewMode,
-    slug: response?.slug || '/',
-    initialData: processedResponse
-  }
 
 }
+
